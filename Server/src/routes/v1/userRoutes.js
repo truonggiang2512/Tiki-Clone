0c3324b5469b078a80c0988ea8c84f858d8b9f0c
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2024-11-07 15:29:32 +0700

    add bcrypt lib and create signup login routes

--- Server/src/routes/v1/userRoutes.js
+++ Server/src/routes/v1/userRoutes.js
@@ -1,11 +1,26 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 const Router = express.Router();
-Router.route('/')
+Router.route('/signup')
   .get((req, res) => {
-    res.status(StatusCodes.OK).json({ message: ' get API user ' })
+    res.status(StatusCodes.OK).json({ message: ' get signup API ' })
   })
   .post((req, res) => {
-    res.status(StatusCodes.CREATED).json({ message: 'create API user ' })
-  })
+    res.status(StatusCodes.CREATED).json({ message: 'create new user ' })
+  }),
+  Router.route('/login')
+    .get((req, res) => {
+      res.status(StatusCodes.OK).json({ message: ' get API user ' })
+    })
+    .post((req, res) => {
+      res.status(StatusCodes.CREATED).json({ message: 'create API user ' })
+    }),
+  Router.route('/logout')
+    .get((req, res) => {
+      res.status(StatusCodes.OK).json({ message: ' get API user ' })
+    })
+    .post((req, res) => {
+      res.status(StatusCodes.CREATED).json({ message: 'create API user ' })
+    })
+
 export const userRoutes = Router
