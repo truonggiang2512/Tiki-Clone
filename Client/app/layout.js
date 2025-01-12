e3f862c20560ec1ced0cc345a45fc2bfa5e20d3d
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2025-01-12 16:23:20 +0700

    merge ui header footer layout

--- Client/app/layout.js
+++ Client/app/layout.js
@@ -1,14 +1,26 @@
-import "./globals.css";
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './globals.css'
 
-export default function RootLayout({ children }) {
+
+export const metadata = {
+  title: 'DokeShop',
+  description: 'Your one-stop e-commerce solution',
+}
+
+export default function RootLayout({
+  children,
+}) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <head>
-        <title>Ecommerce Doke</title>
-      </head>
-      <body>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body >
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
-  );
+  )
 }
+
